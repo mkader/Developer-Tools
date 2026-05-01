14e6da687a7077a6b6806dad614d2e07097bea5e
DayNightSoft <daynightsoft@gmail.com>
2026-05-01 12:04:28 -0500

    Update script.cs

--- Azure Cosmos DB/4_5.dp-420-cosmos-db-dev/09-execute-query-sdk/script.cs
+++ Azure Cosmos DB/4_5.dp-420-cosmos-db-dev/09-execute-query-sdk/script.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using Microsoft.Azure.Cosmos;
+ using System;
+ using Microsoft.Azure.Cosmos;
 
-string endpoint = "<cosmos-endpoint>";
+ string endpoint = "<cosmos-endpoint>";
 
-string key = "<cosmos-key>";
+ string key = "<cosmos-key>";
 
-CosmosClient client = new (endpoint,key);
+ CosmosClient client = new (endpoint, key);
 
-Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
+ Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
 
-Container container = await database.CreateContainerIfNotExistsAsync("products", "/category/name");
+ Container container = await database.CreateContainerIfNotExistsAsync("products", "/category/name");
+
+ string sql = "SELECT * FROM products p";
+ QueryDefinition query = new (sql);
+
+ using FeedIterator<Product> feed = container.GetItemQueryIterator<Product>(
+     queryDefinition: query
+ );
+
+ while (feed.HasMoreResults)
+ {
+     FeedResponse<Product> response = await feed.ReadNextAsync();
+     foreach (Product product in response)
+     {
+         Console.WriteLine($"[{product.id}]\t{product.name,35}\t{product.price,15:C}");
+     }
+ }

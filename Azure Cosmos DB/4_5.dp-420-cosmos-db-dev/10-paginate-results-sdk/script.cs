9215aa4929428e084f7c9a94e4252ee081499bb3
DayNightSoft <daynightsoft@gmail.com>
2026-05-01 12:06:35 -0500

    Update script.cs

--- Azure Cosmos DB/4_5.dp-420-cosmos-db-dev/10-paginate-results-sdk/script.cs
+++ Azure Cosmos DB/4_5.dp-420-cosmos-db-dev/10-paginate-results-sdk/script.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using Microsoft.Azure.Cosmos;
+ using System;
+ using Microsoft.Azure.Cosmos;
 
-string endpoint = "<cosmos-endpoint>";
+ string endpoint = "<cosmos-endpoint>";
 
-string key = "<cosmos-key>";
+ string key = "<cosmos-key>";
 
-CosmosClient client = new CosmosClient(endpoint, key);
+ CosmosClient client = new CosmosClient(endpoint, key);
 
-Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
+ Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
 
-Container container = await database.CreateContainerIfNotExistsAsync("products", "/category/name");
+ Container container = await database.CreateContainerIfNotExistsAsync("products", "/category/name");
+
+ string sql = "SELECT p.id, p.name, p.price FROM products p ";
+ QueryDefinition query = new (sql);
+
+ QueryRequestOptions options = new ();
+ options.MaxItemCount = 50;
+
+ FeedIterator<Product> iterator = container.GetItemQueryIterator<Product>(query, requestOptions: options);
+
+ while (iterator.HasMoreResults)
+ {
+     FeedResponse<Product> products = await iterator.ReadNextAsync();
+     foreach (Product product in products)
+     {
+         Console.WriteLine($"[{product.id}]\t[{product.name,40}]\t[{product.price,10}]");
+     }
+
+     Console.WriteLine("Press any key for next page of results");
+     Console.ReadKey();        
+ }
